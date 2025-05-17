5bcfba7fff38baabaac38377dd9d80680a956eff
Abbas Ismael <aismoctar@gmail.com>
2025-05-18 00:12:55 +0100

    Create fibonacci_series.c

---
+++ fibonacci_series.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+    int limit;
+    scanf("%d", &limit);
+
+    int nb1 = 0, nb2 = 1;
+
+    if (limit >= 0) {
+        printf("%d ", nb1);
+    }
+    if (limit >= 1) {
+        printf("%d ", nb2);
+    }
+
+    int suite = nb1 + nb2;
+    while (suite <= limit) {
+        printf("%d ", suite);
+        nb1 = nb2;
+        nb2 = suite;
+        suite = nb1 + nb2;
+    }
+
+    printf("\n");
+    return 0;
+}

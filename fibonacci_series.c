ecbd9d91f3d8fc1978f35d7b401a7195d0d3d6e7
Abbas Ismael <aismoctar@gmail.com>
2025-05-19 17:50:12 +0100

    Update fibonacci_series.c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -7,18 +7,21 @@ int main(void) {
     if (scanf("%d", &limit) == 1 && limit >= 0) {
         int nb1 = 0, nb2 = 1;
 
-        if (limit > 0) {
+
+        if (limit == 0) {
+            printf("0");
+        } else {
             printf("%d ", nb1);
+
+            while (nb2 <= limit) {
+                printf("%d ", nb2);
+                int suite = nb1 + nb2;
+                nb1 = nb2;
+                nb2 = suite;
+            }
+
         }
 
-        while (nb2 < limit) {
-            printf("%d ", nb2);
-            int suite = nb1 + nb2;
-            nb1 = nb2;
-            nb2 = suite;
-        }
-
-        printf("\n");
     } else {
         printf("Entrez une valeur entière positive valide.\n");
     }

3e0f5ea1396752e33240c8c598972626b8c9f757
Abbas Ismael <aismoctar@gmail.com>
2025-05-19 19:41:06 +0100

    Update fibonacci_series.c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,28 +1,16 @@
 #include <stdio.h>
 
-int main(void) {
-    int limit;
+int main() {
+    int limite;
+    scanf("%d", &limite);
 
-    if (scanf("%d", &limit) == 1 && limit >= 0) {
-        int nb1 = 0, nb2 = 1;
+    int a = 0, b = 1;
 
-
-        if (limit == 0) {
-            printf("0");
-        } else {
-            printf("%d ", nb1);
-
-            while (nb2 <= limit) {
-                printf("%d ", nb2);
-                int suite = nb1 + nb2;
-                nb1 = nb2;
-                nb2 = suite;
-            }
-
-        }
-
-    } else {
-        printf("Entrez une valeur entière positive valide.\n");
+    while (a <= limite) {
+        printf("%d ", a);
+        int temp = a + b;
+        a = b;
+        b = temp;
     }
 
     return 0;

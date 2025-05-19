1f509a01ecc932259cb159f9ed6286269f8d9b8b
Abbas Ismael <aismoctar@gmail.com>
2025-05-19 10:39:07 +0100

    Update fibonacci_series.c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -2,20 +2,25 @@
 
 int main(void) {
     int limit;
-    scanf("%d", &limit);
+    if (scanf("%d", &limit) == 1) {
 
-    int nb1 = 0, nb2 = 1;
+        int nb1 = 0, nb2 = 1;
 
-    int suite = nb1 + nb2;
-    while (suite < limit) {
-        if (limit > 0 && limit > 1) {
-            printf("%d ", nb1);
-            printf("%d ", suite);
-            nb1 = nb2;
-            nb2 = suite;
-            suite = nb1 + nb2;
-            printf("\n");
+        int suite = nb1 + nb2;
+        while (suite < limit) {
+            if (limit > 0 && limit > 1) {
+                printf("%d ", nb1);
+                printf("%d ", suite);
+                nb1 = nb2;
+                nb2 = suite;
+                suite = nb1 + nb2;
+                
+            }
         }
+        printf("\n");
+    }
+    else {
+        printf("Entrez un nombre valide");
     }
 
 

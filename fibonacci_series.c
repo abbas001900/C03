6b0521bc5359da994878cd2d5675da65fdf7889e
Abbas Ismael <aismoctar@gmail.com>
2025-05-20 13:24:53 +0100

    Update fibonacci_series.c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -2,21 +2,20 @@
 
 int main() {
     unsigned long long limite;
-    if (scanf("%llu", &limite) == 1) {
+    if (scanf("%llu", &limite) == 1 && limite >= 0) {
         unsigned long long a = 0, b = 1, f = 0;
 
-        if (limite == 0) {
-            printf("0");
-        } else {
-            printf("0 ");
+        printf("0 ");
+
             while (b <= limite) {
                 printf("%llu ", b);
                 f = a + b;
                 a = b;
                 b = f;
             }
+
         }
-    } else {
+    else {
         printf("Nombre invalide\n");
     }
 

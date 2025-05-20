3e4a9d3ff3adb79dafe2c4c9d88cd16e26161a0f
Abbas Ismael <aismoctar@gmail.com>
2025-05-20 14:14:57 +0100

    Update fibonacci_series.c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
 
-void afficherFibonacci(unsigned long long limite) {
-    unsigned long long a = 0;
-    unsigned long long b = 1;
-
-    printf("%llu ", a);  // affiche le premier terme
-
-    while (b <= limite) {
-        printf("%llu ", b);
-        unsigned long long suivant = a + b;
-        a = b;
-        b = suivant;
-    }
-
-    printf("\n");
-}
-
 int main() {
     unsigned long long limite;
+    unsigned long long a = 0, b = 1;
 
     scanf("%llu", &limite);
+    if (limite >= 0) {
+        printf("%llu ", a);  // afficher le premier terme
 
-    afficherFibonacci(limite);
+        while (b <= limite) {
+            printf("%llu ", b);
+            unsigned long long suivant = a + b;
+            a = b;
+            b = suivant;
+        }
+    }
 
     return 0;
 }

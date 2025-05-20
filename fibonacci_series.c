64d91840ba1fef59c9d0d81606815fb3be09efae
Abbas Ismael <aismoctar@gmail.com>
2025-05-20 14:09:29 +0100

    Update fibonacci_series.c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,45 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+
+void afficherFibonacci(unsigned long long limite) {
+    unsigned long long a = 0;
+    unsigned long long b = 1;
+
+    printf("%llu ", a);  // affiche le premier terme
+
+    while (b <= limite) {
+        printf("%llu ", b);
+        unsigned long long suivant = a + b;
+        a = b;
+        b = suivant;
+    }
+
+    printf("\n");
+}
 
 int main() {
-    char buffer[100];
-    long long limite;
-    char *endptr;
+    unsigned long long limite;
 
-    if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
-        printf("Erreur de lecture\n");
-        return 1;
-    }
+    printf("Entrez la limite : ");
+    scanf("%llu", &limite);
 
-    // Retirer le '\n' final s'il y en a
-    buffer[strcspn(buffer, "\n")] = '\0';
-
-    limite = strtoll(buffer, &endptr, 10);
-
-    // Vérifier si on a bien consommé toute la chaîne (sans espace ni autre caractère)
-    // et que la chaîne n'est pas vide
-    if (endptr == buffer || *endptr != '\0') {
-        printf("Nombre invalide\n");
-        return 1;
-    }
-
-    if (limite < 0) {
-        printf("Erreur : la limite ne peut pas être négative\n");
-        return 1;
-    }
-
-    unsigned long long a = 0, b = 1, f = 0;
-
-    printf("0 ");
-
-    while (b < (unsigned long long)limite) {
-        printf("%llu ", b);
-        f = a + b;
-        a = b;
-        b = f;
-    }
+    afficherFibonacci(limite);
 
     return 0;
 }

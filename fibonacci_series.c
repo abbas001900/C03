f85b090f886a4c779288889556c6ce2d2597bb6f
Abbas Ismael <aismoctar@gmail.com>
2025-05-21 17:22:55 +0100

    Update fibonacci_series.c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,38 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
-int est_entier_valide(const char *s) {
-    for (int i = 0; s[i] != '\0'; i++) {
-        if (!isdigit(s[i]) && s[i] != '\n') {
-            return 0; // caractère non numérique trouvé
-        }
-    }
-    return 1; // tout est bon
-}
 
 int main() {
-    char input[100];
-    fgets(input, sizeof(input), stdin);
+    int n; // nombre de termes à afficher
 
-    // Vérification que ce n’est qu’un entier
-    if (!est_entier_valide(input)) {
-        printf("Erreur : veuillez entrer un entier valide uniquement (ex : 510).\n");
-        return 1;
-    }
-
-    int limite = atoi(input);
+    scanf("%d", &n);
 
     int a = 0, b = 1;
-    printf("%d, ", a);
-    while (b <= limite) {
-        printf("%d ", b);
+
+    printf("Suite de Fibonacci :\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a);
         int temp = a + b;
         a = b;
         b = temp;
     }
-    printf("\n");
 
+    printf("\n");
     return 0;
 }
